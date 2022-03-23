4eb29d49ec3cae3f395bfd6fff24bbde588642c1
Dalton <daltonjnewbrough@gmail.com>
2022-03-22 22:25:46 -0700

    Snapshot from the end of pair programming with Chase. Created code to display the game window

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import pygame
 import os
-pygame.font.init()
-pygame.mixer.init()
+#import time
+#import random
+#pygame.font.init()
+#pygame.mixer.init()
 
+WIDTH, HEIGHT = 1000, 1000
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+BORDER = pygame.Rect(0,0,WIDTH, HEIGHT)
+pygame.display.set_caption('Snea Snakeroo0')
+
+BLACK = (0,0,0)
 #create window
 
+def draw_window():
+	pygame.draw.rect(WIN,BLACK,BORDER)
+	
+def main():
+	
+
+	run = True
+
+	while run:
+		draw_window()
+
+main()
+
+if __name__ == "__main__":
+	main()
