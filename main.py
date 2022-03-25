214c09ab02816f37c6e2aa23a91417cf9ffb520b
Dalton <daltonjnewbrough@gmail.com>
2022-03-25 16:22:35 -0700

    getting on the same page as chase

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import os
 #pygame.font.init()
 #pygame.mixer.init()
 
-WIDTH, HEIGHT = 500, 500
+WIDTH, HEIGHT = 1250, 750
 SPRITE_WIDTH, SPRITE_HEIGHT = 25,25
 HAIR_WIDTH, HAIR_HEIGHT = 50, 50
+POT_WIDTH, POT_HEIGHT = 250, 100
+
+
 VEL = 5
+MEATBALL_VEL = 10
+HAIRNET_VEL = 15
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 BORDER = pygame.Rect(0,0,WIDTH, HEIGHT)
 
 pygame.display.set_caption('Snea Snakeroo0')
 
-#CIRCLES = pygame.image.load(os.path.join('assets', 'circles.png'))
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'pot.png')), (WIDTH, HEIGHT))
+POT = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'pot.png')), (POT_WIDTH, POT_HEIGHT))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'kitchen.png')), (WIDTH, HEIGHT))
 MEATBALL = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'meatball.png')), (SPRITE_WIDTH, SPRITE_HEIGHT))
 HAIR = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'hair.png')), (HAIR_WIDTH, HAIR_HEIGHT))
 
@@ -26,6 +32,7 @@ WHITE = (255,255,255)
 #.set_colorkey makes these backgrounds transparent
 HAIR.set_colorkey(WHITE)
 MEATBALL.set_colorkey(WHITE)
+POT.set_colorkey(WHITE)
 
 #Vince staples loves this because it moves the sprite
 def sprite_handle_movement(keys_pressed, sprite):
@@ -45,6 +52,7 @@ def draw_window(sprite):
 	#WIN.blit(CIRCLES,(WIDTH - CIRCLES.get_width() + 50,0))
 	WIN.blit(MEATBALL, (sprite.x, sprite.y))
 	WIN.blit(HAIR, (200,200))
+	WIN.blit(POT, (0, 500))
 #main function loop
 def main():	
 	
