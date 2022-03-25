0b48f06502232b5f953aa330ea4cbaa540da2896
Chase <Chaseak907@gmail.com>
2022-03-25 18:58:16 -0500

    Added Pot Handle Movement

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import os
 #pygame.font.init()
 #pygame.mixer.init()
 
-WIDTH, HEIGHT = 1250, 750
+WIDTH, HEIGHT = 1250, 600
 SPRITE_WIDTH, SPRITE_HEIGHT = 25,25
 HAIR_WIDTH, HAIR_HEIGHT = 50, 50
 POT_WIDTH, POT_HEIGHT = 250, 100
-
+STOVE_WIDTH, STOVE_HEIGHT = 800, 140
 
 VEL = 5
 MEATBALL_VEL = 10
@@ -24,6 +24,8 @@ POT = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'pot.png')
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'kitchen.png')), (WIDTH, HEIGHT))
 MEATBALL = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'meatball.png')), (SPRITE_WIDTH, SPRITE_HEIGHT))
 HAIR = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'hair.png')), (HAIR_WIDTH, HAIR_HEIGHT))
+STOVE = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'stove.png')),(STOVE_WIDTH, STOVE_HEIGHT))
+
 
 #RGB COLOR KEYS
 BLACK = (0,0,0)
@@ -35,28 +37,28 @@ MEATBALL.set_colorkey(WHITE)
 POT.set_colorkey(WHITE)
 
 #Vince staples loves this because it moves the sprite
-def sprite_handle_movement(keys_pressed, sprite):
-	if keys_pressed[pygame.K_a] and sprite.x - VEL > 0: #LEFT
-		sprite.x -= VEL	
-	if keys_pressed[pygame.K_d] and sprite.x + VEL + sprite.width < WIDTH: #RIGHT
-		sprite.x += VEL	
-	if keys_pressed[pygame.K_w] and sprite.y - VEL > 0: #UP
-		sprite.y -= VEL	
-	if keys_pressed[pygame.K_s] and sprite.y + VEL + sprite.height < HEIGHT: #DOWN
-		sprite.y += VEL	
+def pot_handle_movement(keys_pressed, pot_sprite):
+	if keys_pressed[pygame.K_a] and pot_sprite.x - VEL > 0: #LEFT
+		pot_sprite.x -= VEL	
+	if keys_pressed[pygame.K_d] and pot_sprite.x + VEL + pot_sprite.width < WIDTH: #RIGHT
+		pot_sprite.x += VEL	
+	
 
 #window function
-def draw_window(sprite):
+def draw_window(sprite, pot_sprite):
 	pygame.draw.rect(WIN,BLACK,BORDER)
 	WIN.blit(BACKGROUND, (0,0))
 	#WIN.blit(CIRCLES,(WIDTH - CIRCLES.get_width() + 50,0))
 	WIN.blit(MEATBALL, (sprite.x, sprite.y))
 	WIN.blit(HAIR, (200,200))
-	WIN.blit(POT, (0, 500))
+	WIN.blit(POT, (pot_sprite.x, pot_sprite.y))
+	WIN.blit(STOVE, ((WIDTH/2 - STOVE_WIDTH/2), (HEIGHT - STOVE_HEIGHT)))
 #main function loop
 def main():	
 	
 	sprite = pygame.Rect(300,300, SPRITE_WIDTH, SPRITE_HEIGHT)
+	pot_sprite = pygame.Rect((WIDTH/2 - POT_WIDTH/2), (HEIGHT - STOVE_HEIGHT- POT_HEIGHT), POT_WIDTH, POT_HEIGHT)
+
 
 	FPS = 60
 	clock = pygame.time.Clock()
@@ -70,8 +72,8 @@ def main():
 				run = False
 
 		keys_pressed = pygame.key.get_pressed()
-		sprite_handle_movement(keys_pressed, sprite)
-		draw_window(sprite)
+		pot_handle_movement(keys_pressed, pot_sprite)
+		draw_window(sprite, pot_sprite)
 		pygame.display.update()
 
 if __name__ == "__main__":
